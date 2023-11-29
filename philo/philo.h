712c69dc7df76a54293d3aa2b4b7610778dcf5ec
rabeloguedes <rabeloguedes@proton.me>
2023-11-29 12:43:21 +0000

    init: Initializes the repo.

---
+++ philo/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 12:39:34 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/11/29 12:41:41 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
